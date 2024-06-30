ead709438a6f69a2fc833a4b8c79239d3056d723
AsiaGrpr <groupierre.asia@gmail.com>
2024-06-30 07:45:44 -0700

    try fix

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -6,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import NoResultFound
-from typing import TypeVar
 from user import Base, User
 
 
