a7e920d5f4e4e9dde2bb7299f14062f9ff619ba7
AsiaGrpr <groupierre.asia@gmail.com>
2024-06-30 15:04:12 +0200

    retry all tasks

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,42 +5,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from typing import TypeVar
+
 from user import Base, User
 
 
 class DB:
     """DB class"""
 
-    def __init__(self):
-        """constructor"""
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+    def __init__(self) -> None:
+        """Initialize a new DB instance"""
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
-    @property
-    def _session(self):
-        """create a session"""
-        if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
-            self.__session = DBSession()
-        return self.__session
-
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """save the user to the database and returns a User object"""
-        user = User(email=email, hashed_password=hashed_password)
+    def add_user(self, email: str, hashed_password: str, **kwargs) -> User:
+        """Add a user to the database"""
+        user = User(email=email, hashed_password=hashed_password, **kwargs)
         self._session.add(user)
         self._session.commit()
+
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """found the users table"""
-        if kwargs is None:
-            raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
+        """Find a user in the database"""
+        user = self._session.query(User).filter_by(**kwargs)
+        if user.count() == 0:
             raise NoResultFound
-        return user
+
+        return user.first()
